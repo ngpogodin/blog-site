289cf24d28fb370893861c08c01f187485c8dd85
Andrey Pogodin <ngpogodin@gmail.com>
2021-11-01 16:27:21 +0200

    There are new function

--- models/articleModel.js
+++ models/articleModel.js
@@ -7,21 +7,21 @@ const ArticleSchema = new mongoose.Schema({
     slug: { type: String, unique: true,},
     title: { type: String, require: true, maxLength: 100, trim: true},
     description: { type: String, maxLength: 500, trim: true },
-    body: { type: String, require: true, minlength: 100, trim: true },
+    body: { type: String, require: true,  trim: true },
     tagList: [String],
     createdAt: Date,
     updatedAt: Date,
-    favorited: false,
     favoritesCount: { type: Number, default: 0 },
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
   }
 )
 
 
-ArticleSchema.pre('save', (next) => {
+ArticleSchema.pre('save',function(next) {
     this.slug = slug(this.title);
     next();
 })
+
 
 // + middleware for findOneAndUpdate()
 
